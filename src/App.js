1a85378efdad82a745a9d593ae0d20233c0dbdd8
piyussh <7piyush77@gmail.com>
2025-01-24 01:51:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
+import './App.css';
+import UserCard from './UserCard';
+import { UserStatusProvider } from './userContextStatus';
+import Home from './Home';
+import UserList from './UserList';
+import About from './About';
+
+function App() {
+  return (
+    <UserStatusProvider>
+      <Router>
+
+      <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/">Cards</Link></li>
+          <li><Link to="/users">User List</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={
+          <div>
+          <h1>Hello, React!</h1>
+          <p>This is my first React app.</p>
+          <UserCard name='Person1' email='person1@gmail.com'/>
+          <UserCard name='Person2' email='person2@gmail.com'/>
+        </div>
+        } />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/users" element={<UserList/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+
+      </Router>
+    </UserStatusProvider>
+    
+  );
+}
+
+export default App;
