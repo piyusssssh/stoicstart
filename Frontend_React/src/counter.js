04e8b43c5f52c194dc4b75df1067269daf385898
piyussssh <7piyush77@gmail.com>
2025-01-25 03:31:44 +0530

    Created counter component from consuming redux and updating state based on dispatches

---
+++ Frontend_React/src/counter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './redux/counterSlice';
+
+const Counter = ()=>{
+
+    const count = useSelector((state)=>state.counter.value);
+    const dispatch = useDispatch();
+
+
+    return(
+        <div style={counterStyle}>
+        <h2>Redux Counter</h2>
+        <p>count: {count}</p>
+        <button onClick={dispatch(increment())}>Increment</button>
+        <button onClick={dispatch(decrement())}>Decrement</button>
+        <button onClick={dispatch(reset())}>Reset</button>
+    </div>
+    );
+}
+
+const counterStyle = {
+    textAlign: 'center',
+    margin: '20px',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '10px',
+    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)',
+}
+
+export default Counter;
